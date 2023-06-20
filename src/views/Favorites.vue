cf005cb1e5451df87079b91e3919b0235a2000bd
vidakovicrobert <vidakovic.robert19@gmail.com>
2023-06-20 11:29:54 +0200

    Update Favorites.vue

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -3,28 +3,28 @@
         <v-container>
             <v-card-title>Favorite Songs</v-card-title>
             <v-list>
-                <v-list-item v-for="(song, index) in getFavoriteSongs()" :key="index">
-                    <v-list-item-content>
-                        <v-row>
-                            <v-col cols="2">
-                                <v-img :src="song.src" height="60" contain></v-img>
-                            </v-col>
-                            <v-col cols="9">
-                                <v-list-item-content>
-                                    <v-list-item-title class="purple--text">{{ song.name }}</v-list-item-title>
-                                    <v-list-item-subtitle class="grey--text text--darken-1">{{ song.artistName }} - {{
-                                        song.albumName }} ({{ song.year }})</v-list-item-subtitle>
-                                </v-list-item-content>
-                            </v-col>
-                            <v-col cols="1">
-                                <v-btn icon :class="{ 'purple--text': isFavorite(song.id) }"
-                                    @click.stop="toggleFavorite(song.id)">
-                                    <v-icon>{{ isFavorite(song.id) ? 'mdi-heart' : 'mdi-heart-outline' }}</v-icon>
-                                </v-btn>
+                <v-list-item v-for="song in favorites" :key="song.id">
 
-                            </v-col>
-                        </v-row>
-                    </v-list-item-content>
+                    <v-row align="center">
+                        <v-col cols="2">
+                            <v-img :src="song.src" height="60" contain></v-img>
+                        </v-col>
+                        <v-col cols="6">
+                            <v-list-item-content>
+                                <v-list-item-title class="purple--text">{{ song.name }}</v-list-item-title>
+                                <v-list-item-subtitle class="grey--text text--darken-1">{{ song.artistName }} - {{
+                                    song.albumName }} ({{ song.year }})</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-col>
+                        <v-col cols="1">
+                            <v-btn icon x-small :class="{ 'purple--text': isFavorite(song.id) }"
+                                @click="removeFromFavorites(song.id)">
+                                <v-icon>{{ isFavorite(song.id) ? 'mdi-heart' : 'mdi-heart-outline' }}</v-icon>
+                            </v-btn>
+
+                        </v-col>
+                    </v-row>
+
                 </v-list-item>
             </v-list>
         </v-container>
@@ -32,25 +32,50 @@
 </template>
   
 <script>
+import { db, auth } from '@/firebase';
+import { collection, doc, deleteDoc, onSnapshot } from 'firebase/firestore';
+
 export default {
     name: 'Favorites',
 
-    props: {
-        list: Array,
-        favorites: Array
+    data() {
+        return {
+            favorites: [],
+        };
+    },
+    created() {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                const userFavoritesRef = collection(db, 'users', user.uid, 'favorites');
+                onSnapshot(userFavoritesRef, (snapshot) => {
+                    if (!snapshot.empty) {
+                        this.favorites = snapshot.docs.map((doc) => doc.data());
+                    }
+                });
+            } else {
+                this.favorites = [];
+            }
+        });
     },
     methods: {
-        getFavoriteSongs() {
-            return this.list.filter(song => this.favorites.includes(song.id));
-        },
-        toggleFavorite(songId) {
-            // Access the toggleFavorite method from the parent component
-            this.$emit('toggle-favorite', songId);
+        removeFromFavorites(songId) {
+            const user = auth.currentUser;
+            if (user) {
+                const favoritesDocRef = doc(db, 'users', user.uid, 'favorites', songId);
+                deleteDoc(favoritesDocRef)
+                    .then(() => {
+                        console.log('Removed from favorites successfully');
+                    })
+                    .catch((error) => {
+                        console.error('Error removing from favorites:', error);
+                    });
+            } else {
+                console.error('User is not authenticated');
+            }
         },
         isFavorite(songId) {
-            // Access the isFavorite method from the parent component
-            return this.favorites.includes(songId);
-        }
+            return this.favorites.some((song) => song.id === songId);
+        },
     },
 };
 </script>
