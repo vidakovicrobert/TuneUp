47a0d78f8b5ed370940dfb0cfcbc07b237b4abd0
vidakovicrobert <vidakovic.robert19@gmail.com>
2023-06-21 14:08:13 +0200

    style and array removal

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -1,32 +1,38 @@
 <template>
-    <v-card style="background-color: black;">
-        <v-container>
-            <v-card-title><span class="purple--text">Favorite Songs</span></v-card-title>
-            <v-list style="background-color: #292929;">
-                <v-list-item v-for="song in favorites" :key="song.id">
-
-                    <v-row align="center">
-                        <v-col cols="2">
-                            <v-img :src="song.src" height="60" contain></v-img>
-                        </v-col>
-                        <v-col cols="6">
-                            <v-list-item-content>
-                                <v-list-item-title class="purple--text">{{ song.name }}</v-list-item-title>
-                                <v-list-item-subtitle class="grey--text text--darken-1">{{ song.artistName }} - {{
-                                    song.albumName }} ({{ song.year }})</v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-col>
-                        <v-col cols="1">
-                            <v-btn icon x-small :class="{ 'purple--text': isFavorite(song.id) }"
-                                @click="removeFromFavorites(song.id)">
-                                <v-icon>{{ isFavorite(song.id) ? 'mdi-heart' : 'mdi-heart-outline' }}</v-icon>
-                            </v-btn>
-
-                        </v-col>
-                    </v-row>
-
-                </v-list-item>
-            </v-list>
+    <v-card style="background-color: black;" dark>
+        <v-container class="background">
+            <v-row align="center" justify="space-between">
+                <v-col cols="auto">
+                    <v-card-title><span class="purple--text">Favorite Songs</span></v-card-title>
+                </v-col>
+            </v-row>
+            <v-card class="card-border" outlined v-if="favorites.length > 0">
+                <v-list align="left" style="max-height: 500px; overflow-y: auto; overflow-x: hidden;">
+                    <v-list-item v-for="song in favorites" :key="song.id">
+                        <v-row align="center">
+                            <v-col cols="2">
+                                <v-img :src="song.src" height="60" contain></v-img>
+                            </v-col>
+                            <v-col cols="5">
+                                <v-list-item-content>
+                                    <v-list-item-title class="purple--text">{{ song.name }}</v-list-item-title>
+                                    <v-list-item-subtitle class="grey--text text--darken-1">{{ song.artistName }} - {{
+                                        song.albumName }} ({{ song.year }})</v-list-item-subtitle>
+                                </v-list-item-content>
+                            </v-col>
+                            <v-col cols="2">
+                                <v-btn icon x-small :class="{ 'purple--text': isFavorite(song.id) }"
+                                    @click="removeFromFavorites(song.id)">
+                                    <v-icon>{{ isFavorite(song.id) ? 'mdi-heart' : 'mdi-heart-outline' }}</v-icon>
+                                </v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-list-item>
+                </v-list>
+            </v-card>
+            <v-card v-else>
+                <v-card-text>No favorite songs found.</v-card-text>
+            </v-card>
         </v-container>
     </v-card>
 </template>
@@ -37,7 +43,6 @@ import { collection, doc, deleteDoc, onSnapshot } from 'firebase/firestore';
 
 export default {
     name: 'Favorites',
-
     data() {
         return {
             favorites: [],
@@ -64,6 +69,8 @@ export default {
                 const favoritesDocRef = doc(db, 'users', user.uid, 'favorites', songId);
                 deleteDoc(favoritesDocRef)
                     .then(() => {
+                        // Remove the song from the favorites array
+                        this.favorites = this.favorites.filter((song) => song.id !== songId);
                         console.log('Removed from favorites successfully');
                     })
                     .catch((error) => {
